bf19c65a5c9f9e23f2fd21f279be5a4ba61a2208
Davide Nastri <d.nastri@gmail.com>
2022-01-13 21:36:23 +0100

    feat: Added wake_word_detection.py (offline)

--- voice_assistant.py
+++ voice_assistant.py
@@ -7,10 +7,9 @@ import pyjokes
 
 listener = sr.Recognizer()
 engine = pyttsx3.init()
+engine.setProperty('rate', 145)
 voices = engine.getProperty('voices')
-
 engine.runAndWait()
-
 engine.setProperty('voice', voices[0].id)
 
 
@@ -21,44 +20,47 @@ def talk(text):
 
 def take_command():
     with sr.Microphone() as source:
-        print('listening...')
-        voice = listener.listen(source)
-        command = listener.recognize_google(voice)
-        # La riga sotto abilita l'italiano
-        # command = listener.recognize_google(voice, language='it-IT')
+        listener.adjust_for_ambient_noise(source)
+        talk('I am listening.')
+        try:
+            # Timeout dopo 5 secondi di silenzio
+            # Frase massima 20 secondi
+            voice = listener.listen(source, 4, 20)
+            command = listener.recognize_google(voice, language='en-US')
+        except sr.UnknownValueError:
+            print("Oops! Didn't catch that")
+            command = ""
+        except sr.RequestError as e:
+            print("Uh oh! Couldn't request results from Google Speech Recognition service; {0}".format(e))
+            command = ""
         command = command.lower()
-        if 'alexa' in command:
-            command = command.replace('alexa', '')
-            print(command)
     return command
 
 
-def run_alexa():
+def run_assistant():
     command = take_command()
     print(command)
-    if 'play' in command:
-        song = command.replace('play', '')
-        talk('playing ' + song)
-        pywhatkit.playonyt(song)
-    elif 'time' in command:
-        time = datetime.datetime.now().strftime('%I:%M %p')
-        talk('Current time is ' + time)
-    elif 'who the heck is' in command:
-        person = command.replace('who the heck is', '')
-        info = wikipedia.summary(person, 1)
-        print(info)
-        talk(info)
-    elif 'date' in command:
-        talk('sorry, I have a headache')
-    elif 'are you single' in command:
-        talk('I am in a relationship with wifi')
-    elif 'joke' in command:
-        talk(pyjokes.get_joke())
-    elif 'hello' in command:
-        talk("Hello, my friend!")
-    else:
-        talk('Please say the command again.')
+    if command != "":
+        if 'play' in command:
+            song = command.replace('play', '')
+            talk('playing ' + song)
+            pywhatkit.playonyt(song)
+        elif 'time' in command:
+            current_time = datetime.datetime.now().strftime('%I:%M %p')
+            talk('Current time is ' + current_time)
+        elif 'who is' in command:
+            person = command.replace('who is', '')
+            info = wikipedia.summary(person, 1)
+            print(info)
+            talk(info)
+        elif 'date' in command:
+            talk('sorry, I have a headache')
+        elif 'are you single' in command:
+            talk('I am in a relationship with a WiFi router')
+        elif 'joke' in command:
+            talk(pyjokes.get_joke())
+        elif 'hello' in command:
+            talk("Hello, my friend!")
+        else:
+            talk('I did not understand.')
 
-
-while True:
-    run_alexa()
